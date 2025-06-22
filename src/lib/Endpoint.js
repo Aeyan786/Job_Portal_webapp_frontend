c17d210a340df4bb1eebddcba779717c2e6335b8
Aeyan-Raza-pk <www.aeyanraza65@gmail.com>
2025-06-22 23:04:09 +0500

    add files

--- src/lib/Endpoint.js
+++ src/lib/Endpoint.js
@@ -1,4 +1,4 @@
-export const  USER_END_POINT = "http://localhost:5000/api/user"
-export const  JOB_END_POINT = "http://localhost:5000/api/job"
-export const  APPLICATION_END_POINT = "http://localhost:5000/api/application"
-export const  COMPANY_END_POINT = "http://localhost:5000/api/company"
+export const  USER_END_POINT = "https://jobportalwebappbackend-production.up.railway.app/api/user"
+export const  JOB_END_POINT = "https://jobportalwebappbackend-production.up.railway.app/api/job"
+export const  APPLICATION_END_POINT = "https://jobportalwebappbackend-production.up.railway.app/api/application"
+export const  COMPANY_END_POINT = "https://jobportalwebappbackend-production.up.railway.app/api/company"
